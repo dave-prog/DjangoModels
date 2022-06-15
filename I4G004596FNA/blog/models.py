74549d5d1644e64d8d8e65a4d891407e8abeee29
David <davidolanrewaju538@gmail.com>
2022-06-15 23:39:38 +0100

    Post Model Created

--- I4G004596FNA/blog/models.py
+++ I4G004596FNA/blog/models.py
@@ -1,3 +1,12 @@
+from cgitb import text
 from django.db import models
+from django.forms import DateField
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length = 200)
+    text = models.TextField()
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    created_date = models.DateField()
+    published_date = models.DateField()
